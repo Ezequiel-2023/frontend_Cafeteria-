f70902be7be8ddfe50255d04065db98f1a6d5cbf
Ezequiel Rivera <moiseslarios515@gmail.com>
2024-10-25 21:27:06 -0600

    fronted agregar usuario

--- src/component/pages/AdminPanel/agregarUsuario/AgregarUsuario.js
+++ src/component/pages/AdminPanel/agregarUsuario/AgregarUsuario.js
@@ -1,7 +1,10 @@
-import { Container, Row, Col,  } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import './agregarUsuario.scss'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import IconoUsuario from '../../../../assets/img/icono_usuario.png'
 
 function AgregarUsuario() {
   return (
@@ -22,7 +25,41 @@ function AgregarUsuario() {
         {/* aqui enpieza lo que esta fuera de la navegacion*/}
         <Col xs={9} className="content-column">
           <div>
-            <h1>hola desde agregarUsuario</h1>
+          <div className="rounded">
+            <Image src={IconoUsuario} rounded className="login-logo" />
+          </div>
+            <Form>
+             <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Nombre</Form.Label>
+                  <Form.Control type="text" placeholder="Enter email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Apellido</Form.Label>
+                  <Form.Control type="text" placeholder="Enter email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Ingrese el  correo</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                      <Form.Text className="text-muted">
+                       correo segun rol del usuario 
+                      </Form.Text>
+              </Form.Group>
+
+               <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Select aria-label="Default select example">
+                  <option> seleccione un rol</option>
+                  <option value="1">Administrador</option>
+                  <option value="2">Empleado</option>
+                  <option value="3">Estuddiante</option>
+                </Form.Select>
+              <Button variant="primary" type="submit">
+               Submit
+              </Button>
+             </Form>
           </div>   
         </Col>
       </Row>
